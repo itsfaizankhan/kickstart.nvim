8d7675fd5b89bbd7a3ba492926d147931632d329
Faizan Khan <71210312+ATOMICMAN007@users.noreply.github.com>
2024-09-23 15:33:05 +0530

    Opening list of existing buffers now defaults to normal mode for easier navigation.
    Changed interface style of fuzzy finding in current buffer for better readability.

--- init.lua
+++ init.lua
@@ -414,12 +414,23 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+
+      -- [[ CUSTOM Find existing buffers in normal mode - modified ]]
+
+      vim.keymap.set('n', '<leader><leader>', function()
+        builtin.buffers { on_complete = {
+          function()
+            vim.cmd 'stopinsert'
+          end,
+        } }
+      end, { desc = '[ ] Find existing buffers' })
+
+      -- [[ CUSTOM ]]
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_ivy {
           winblend = 10,
           previewer = false,
         })
